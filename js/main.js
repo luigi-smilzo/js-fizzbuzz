044086bc82f0fcd894689ad25bc5678440533c24
Luigi <luigismilzo@live.com>
2020-04-01 16:50:17 +0200

    Fix

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ per i multipli di 5 stampi Buzz.
 per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
 for (var i = 1; i < 101; i++) {
-    if ( i % 3 == 0 && i % 5 !== 0 ) {
-        console.log('Fizz');
-    } else if ( i % 5 == 0 && i % 3 !==0) {
-        console.log('Buzz');
-    } else if ( i % 3 == 0 && i % 5 == 0) {
+    if ( i % 3 == 0 && i % 5 == 0 ) {
         console.log('FizzBuzz');
+    } else if ( i % 5 == 0) {
+        console.log('Buzz');
+    } else if ( i % 3 == 0) {
+        console.log('Fizz');
     } else {
         console.log(i);
     }
